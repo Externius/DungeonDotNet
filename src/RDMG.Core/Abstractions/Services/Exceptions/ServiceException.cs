2ea925152640aa12666d4f65f7cf8c3deaf9ac59
Externius <t.andras@outlook.com>
2023-12-09 17:25:09 +0100

    Update to .net8 + update pkgs

--- src/RDMG.Core/Abstractions/Services/Exceptions/ServiceException.cs
+++ src/RDMG.Core/Abstractions/Services/Exceptions/ServiceException.cs
@@ -1,24 +1,18 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace RDMG.Core.Abstractions.Services.Exceptions;
 
-[Serializable]
 public class ServiceException : Exception
 {
     public const string GeneralError = "GeneralError";
     public const string GeneralAggregateError = "GeneralAggregateError";
-    
+
     public ServiceException() : base(GeneralError)
     {
     }
 
     public ServiceException(string message)
         : base(message)
-    {
-    }
-
-    protected ServiceException(SerializationInfo info, StreamingContext context) : base(info, context)
     {
     }
 }
